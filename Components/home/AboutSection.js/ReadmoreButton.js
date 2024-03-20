d75357bc69e8a47d376ea9747d90bb5f414d538e
mhamad raad <hamaraad883@gmail.com>
2024-03-20 22:59:34 +0300

    fix navbar animation

--- Components/home/AboutSection.js/ReadmoreButton.js
+++ Components/home/AboutSection.js/ReadmoreButton.js
@@ -6,10 +6,10 @@ const ReadmoreButton = () => {
   return (
         <button
           type="button"
-          className="flex xl:text-[22px] lg:text-[20px] lg:gap-[20px] gap-[10px] text-[10px] md:mt-[50px] sm:mt-[25px] justify-center items-center lg:py-[15px] lg:px-[31px] sm:px-[16px] sm:py-[6px] bg-white text-[#2b3c40] font-semibold rounded-[10px] md:text-[12px] md:gap-[13px] md:py-[12px] md:px-[19px]"
+          className="flex xl:text-[22px] lg:text-[20px] lg:gap-[20px] gap-[10px] text-[10px] md:mt-[50px] sm:mt-[25px] justify-center items-center lg:py-[15px] lg:px-[31px] sm:px-[16px] sm:py-[6px] bg-white text-[#2b3c40] font-semibold rounded-[10px] md:text-[16px] md:gap-[13px] md:py-[12px] md:px-[19px]"
         >
           Learn More
-          <HiArrowNarrowRight className="lg:text-[32px] text-[18px]" />
+          <HiArrowNarrowRight className="md:text-[32px]" />
         </button>
   );
 }
