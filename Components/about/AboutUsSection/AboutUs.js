ff4aacf44c01fa003bb3cf95c4e2cfafe611858c
mhamad raad <hamaraad883@gmail.com>
2024-03-22 20:56:02 +0300

    fix design buggs

--- Components/about/AboutUsSection/AboutUs.js
+++ Components/about/AboutUsSection/AboutUs.js
@@ -4,17 +4,12 @@ import Underline from '@/assets/about/Underline';
 
 const AboutUs = () => {
   return (
-    <section className="lg:mt-[185px] xl:px-[111px] md:px-[60px] xl:py-[90px] py-[75px] flex flex-col bg-[#1588ff] md:mt-[125px] sm:mt-[80px] sm:px-[40px]">
-      <div className="flex flex-col justify-center items-center">
-        <h2 className="xl:text-[60px] md:text-[48px] text-center font-extrabold text-white md:tracking-[3.2px] sm:text-[24px] sm:tracking-[1.2px]">
-          We do things{' '}
-          <span className="relative xl:text-[60px] md:text-[48px] text-center font-extrabold text-white md:tracking-[3.2px] sm:text-[24px] sm:tracking-[1.2px]">
-            differently
-            <div className="xl:ml-[63%] ml-[60%]">
-              <Underline className="md:ml-[-33px] sm:ml-[-8px]" />
-            </div>
-          </span>
+    <section className="lg:mt-[185px] xl:px-[111px] md:px-[60px] xl:py-[90px] py-[75px] flex flex-col items-center bg-[#1588ff] md:mt-[125px] sm:mt-[80px] sm:px-[20px]">
+      <div className='flex flex-col items-end justify-center'>
+        <h2 className="xl:text-[60px] md:text-[32px] text-center font-extrabold text-white md:tracking-[3.2px] sm:text-[18px] sm:tracking-[1.2px] flex">
+          We do things differently
         </h2>
+          <Underline className="xl:w-[360px] md:w-[230px] sm:w-[120px]"/>
       </div>
       <AboutList />
     </section>
--- Components/about/HeroSection.js
+++ Components/about/HeroSection.js
@@ -5,7 +5,7 @@ import TitleUnderline from '@/assets/about/TitleUnderline';
 
 const HeroSection = () => {
   return (
-    <section className="xl:px-[111px] md:px-[60px] sm:pl-[24px] sm:pr-[10px] mt-[180px] relative flex items-center justify-between ">
+    <section className="xl:px-[111px] md:px-[60px] sm:pl-[24px] md:pr-[25px] sm:pr-[10px] mt-[180px] relative flex md:flex-row sm:flex-col items-center justify-between sm:gap-[75px]">
       <div className="absolute xl:left-[-260px] lg:left-[-220px] md:top-[100px] md:left-[-140px] sm:top-[30px] sm:left-[-105px]">
         <div className="rounded-full xl:w-[574px] xl:h-[574px] lg:w-[448px] lg:h-[448px] border-2 border-[#42a9e433] flex justify-center items-center md:w-[304px] md:h-[304px] sm:w-[178px] sm:h-[178px]">
           <div className="rounded-full xl:w-[479px] xl:h-[479px] lg:w-[374px] lg:h-[374px] border-2 border-[#42a9e433] flex justify-center items-center md:w-[254px] md:h-[254px] sm:w-[148px] sm:h-[148px]">
@@ -13,7 +13,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col md:w-[55%] sm:w-[205px]">
+      <div className="flex flex-col sm:align-center md:w-[55%] sm:w-[205px]">
         <div className="w-full">
           <h1 className="text-[#1588ff] xl:text-[200px] lg:text-[128px] font-extrabold lg:tracking-[2px] lg:leading-[100px] text-left md:text-[96px] md:tracking-[1px] xl:leading-[160px] md:leading-[70px] sm:text-[32px] sm:tracking-[1.6px] sm:leading-[30px]">
             We are
@@ -23,12 +23,12 @@ const HeroSection = () => {
           </h1>
           <TitleUnderline className="lg:ml-[-10px] xl:ml-[290px] md:ml-[-60px] sm:ml-[-45px]" />
         </div>
-        <p className="lg:mt-[87px] xl:text-[28px] lg:text-[20px] md:text-[14px] md:mt-[40px] sm:mt-[16px] sm:text-[10px]">
+        <p className="lg:mt-[87px] xl:text-[28px] lg:text-[20px] md:text-[14px] md:mt-[40px] sm:mt-[16px] sm:text-[10px] sm:text-center md:text-left">
           Glacier Capital, based in Dubai, is a leading force in digital asset management. Founded in 2023, we quickly
           became crypto asset management leaders, driven by innovation, integrity, and transparency.
         </p>
       </div>
-      <div className="xl:w-[524px] xl:h-[600px] lg:w-[408px] lg:h-[453px] relative bg-gradient-to-br from-[#1588ff] to-[#cfeeff] md:rounded-tr-[175px] md:rounded-bl-[175px] md:mr-[31px] md:h-[400px] md:w-[350px] sm:w-[150px] sm:h-[160px] sm:rounded-tr-[60px] sm:rounded-bl-[60px] sm:mr-[20px] lg:mt-[-125px] xl:mt-[-250px]">
+      <div className="xl:w-[524px] xl:h-[600px] lg:w-[408px] lg:h-[453px] relative bg-gradient-to-br from-[#1588ff] to-[#cfeeff] md:rounded-tr-[175px] md:rounded-bl-[175px] md:mr-[31px] md:h-[400px] md:w-[350px] sm:w-[150px] sm:h-[160px] sm:rounded-tr-[60px] sm:rounded-bl-[60px] sm:mr-[20px]">
         <Image
           src={Hero}
           className="absolute md:w-full md:h-full md:rounded-tr-[175px] md:rounded-bl-[175px] md:bottom-[28px] md:left-[31px] sm:w-[150px] sm:h-[160px] sm:rounded-tr-[60px] sm:rounded-bl-[60px] sm:left-[20px] sm:bottom-[18px]"
