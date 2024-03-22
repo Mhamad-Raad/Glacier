4176e36a41bd41e1221e5eff911ce937cb4e6d1f
mhamad raad <hamaraad883@gmail.com>
2024-03-22 21:28:11 +0300

    fix design buggs in team section

--- Components/about/Benifits/BenifitList.js
+++ Components/about/Benifits/BenifitList.js
@@ -8,17 +8,17 @@ import VLine from '@/assets/about/VLine.png';
 
 const BenifitList = () => {
   return (
-    <div className="flex md:flex-row sm:flex-col lg:gap-[69px] md:py-[66px] md:px-[75px] mt-[25px] bg-gradient-to-br from-[#1588ff] to-[#cfeeff] rounded-[15px] z-10 md:gap-[90px] sm:py-[53px] sm:px-[51px] sm:gap-[30px]">
-      <div className="flex flex-col md:gap-[95px] justify-between sm:gap-[55px] md:w-[45%]">
+    <div className="flex md:flex-row sm:flex-col lg:gap-[69px] md:py-[66px] md:px-[40px] mt-[25px] bg-gradient-to-br from-[#1588ff] to-[#cfeeff] rounded-[15px] z-10 md:gap-[50px] sm:py-[53px] sm:px-[51px] sm:gap-[30px]">
+      <div className="flex flex-col gap-[50px] justify-between md:w-[50%]">
         <BenifitItem info={BenifitsData[0]} />
         <BenifitItem info={BenifitsData[1]} />
       </div>
       <Image
         src={VLine}
         alt=""
-        className="md:w-[8px] md:h-[600px] sm:w-0 sm:h-0 lg:w-[13px] lg:h-[708px] lg:mt-[25px]"
+        className="md:w-[8px] md:h-[250px] sm:w-0 sm:h-0 lg:w-[13px] lg:h-[508px] lg:mt-[25px]"
       />
-      <div className="flex flex-col md:gap-[70px] justify-between sm:gap-[55px] md:w-[45%]">
+      <div className="flex flex-col justify-between sm:gap-[55px] md:w-[50%]">
         <BenifitItem info={BenifitsData[2]} />
       </div>
     </div>
--- data/about/BenifitsData.js
+++ data/about/BenifitsData.js
@@ -2,17 +2,17 @@ const BenifitsData = [
   {
     title: 'Spot BTC & ETH',
     description:
-      "Dive into the world of leading cryptocurrencies, Bitcoin (BTC) and Ethereum (ETH), with Glacier Capital's personalized Dollar-Cost Averaging (DCA) service. At Glacier Capital, we offer a tailored solution to help our clients accumulate BTC and ETH systematically based on their unique preferences and financial goals. With our DCA service, our clients can invest in these digital assets at current spot prices, strategically harnessing market fluctuations while minimizing the impact of volatility. Our approach involves investing fixed amounts at intervals customized to the needs of our clients, allowing them to navigate market dynamics effectively.",
+      "Dive into the world of leading cryptocurrencies, Bitcoin (BTC) and Ethereum (ETH), with Glacier Capital's personalized Dollar-Cost Averaging (DCA) service.",
   },
   {
     title: 'Customized solution',
     description:
-      "Uncover a completely tailored investment solution that aligns precisely with your individual investment objectives, risk tolerance, and unique preferences. At Glacier Capital, our team of experts will craft a personalized portfolio that expertly translates your long-term vision into a strategic investment plan. As a discerning investor, you'll gain exclusive access to our strategic advisory services, designed to keep you well-informed about the latest market trends, high-profile opportunities, and potential risks. With Glacier Capital's customized solutions, your investment journey will be precisely designed to meet your needs and aspirations, ensuring that your financial future is in capable hands.",
+      "Uncover a completely tailored investment solution that aligns precisely with your individual investment objectives, risk tolerance, and unique preferences.",
   },
   {
-    title: 'Spot BTC & ETH + Altcoins',
+    title: 'Spot BTC & ETH',
     description:
-      'Embark on a comprehensive cryptocurrency investment journey with our personalized Dollar-Cost Averaging (DCA) service, extending beyond Bitcoin (BTC) and Ethereum (ETH). At Glacier Capital, we offer a bespoke DCA strategy that caters to your unique investment preferences, incorporating a diverse range of altcoins alongside BTC and ETH. With our DCA service, you can invest in various altcoins, ensuring that your cryptocurrency holdings align with your financial objectives.',
+      'Embark on a comprehensive cryptocurrency investment journey with our personalized Dollar-Cost Averaging (DCA) service, extending beyond Bitcoin (BTC) and Ethereum (ETH).',
   },
 ];
 
